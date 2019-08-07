e3e64f7dd194cade6542bc42ba2d4852afcdf288
ShallowLan <15669147150@163.com>
2019-08-07 22:08:25 +0800

    完善写文章逻辑

--- src/pages/write/index.js
+++ src/pages/write/index.js
@@ -1,33 +1,78 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Editor from 'for-editor';
 import {
-  WriteWrapper
+  WriteWrapper,
+  WriteBackHome,
+  PublishArticles
 } from './style';
 
 class Home extends PureComponent {
   constructor() {
-    super()
+    super();
     this.state = {
-      value: ''
+      value: '',
+      toolbar: {
+        h1: true, // h1
+        h2: true, // h2
+        h3: true, // h3
+        h4: true, // h4
+        img: true, // 图片
+        link: true, // 链接
+        code: true, // 代码块
+        preview: true, // 预览
+        expand: false, // 全屏
+        undo: true, // 撤销
+        redo: true, // 重做
+        save: true, // 保存
+        subfield: true, // 单双栏模式
+      },
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleOnSave = this.handleOnSave.bind(this);
   }
   
   render() {
     if (!this.props.whetherSignIn) {
       return <Redirect to="/sign_in"/>
     }
+    const { value, toolbar } = this.state;
     return (
       <WriteWrapper>
+        <WriteBackHome>
+          <input placeholder="请输入文章标题..."/>
+          <PublishArticles>
+            <Link to="/">
+              <span className="back-home">回首页</span>
+            </Link>
+            <span>发布</span>
+            <img alt="" src="https://user-gold-cdn.xitu.io/2019/3/18/1699037a6f8c50ee?imageView2/1/w/64/h/64/q/85/interlace/1"/>
+          </PublishArticles>
+        </WriteBackHome>
+        <Editor
+          value={value}
+          placeholder="开始你的创作..."
+          toolbar={toolbar}
+          lineNum={false}
+          preview={true}
+          subfield={true}
+          onChange={this.handleChange}
+          onSave={this.handleOnSave}
+        />
       </WriteWrapper>
     )
-
   }
 
   handleChange(value) {
     this.setState({
       value
     })
+  }
+
+  handleOnSave(e) {
+    console.log(this.state.value);
   }
 
 }
--- src/pages/write/style.js
+++ src/pages/write/style.js
@@ -3,6 +3,57 @@ import styled from 'styled-components';
 export const WriteWrapper = styled.div`
   width: 100%;
   min-height: 750px;
+  display: flex;
+  flex-direction: column;
   position: absolute;
   background-color: #FFF;
+  .for-container{
+    flex: 1;
+    border-radius: 0px;
+  }
+  .for-editor-edit {
+    border-right: 1px solid #ddd;
+  }
+  .for-editor {
+    height: 115%;
+  }
+`
+export const WriteBackHome = styled.div`
+  width: 100%;
+  height: 64px;
+  background-color: #FFF;
+  display: flex;
+  justify-content: space-between;
+  input {
+    width: 1000px;
+    height: 64px;
+    color: #000;
+    font-size: 24px;
+    font-weight: 700;
+    padding-left: 20px;
+    border: none;
+    outline: none;
+  }
+`
+export const PublishArticles = styled.div`
+  width: 200px;
+  height: 64px;
+  display: flex;
+  align-items: center;
+  span {
+    font-size: 16px;
+    white-space: nowrap;
+    color: #007fff;
+    cursor: pointer;
+  }
+  .back-home {
+    display: block;
+    margin-right: 20px;
+  }
+  img {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    margin-left: 30px;
+  }
 `
